ace502747a361f2c234f11734f21380e3864fa46
unknown <ikrambouaddi11@gmail.com>
2023-04-07 01:52:54 +0200

    new add

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const currData = document.querySelector('.curr-Date'),
 daysTag = document.querySelector('.days'),
 prevIconsNext = document.querySelectorAll('.icons span');
+
+
+let date = new Date(),
